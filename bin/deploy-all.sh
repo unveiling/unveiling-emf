bbb8234f4078fd1ff8c666e3b0f4349fee1d3dd7
Gerson Sunyé <sunye@protonmail.com>
2020-10-12 10:15:49 +0200

    Add script for releasing several versions at the same time

--- bin/deploy-all.sh
+++ bin/deploy-all.sh
@@ -4,10 +4,9 @@ versions="2.15.0 2.16.0 2.17.0 2.18.0 2.19.0 2.20.0 2.21.0 2.22.0 2.23.0"
 
 for each in $versions; do
 	echo "Release version ${each}"
-	#mvn versions:set -DnewVersion=${each} -DautoVersionSubmodules=true
-	#mvn -f unveil-emf-repackaged tycho-versions:set-version -D newVersion=${each}
-	#mvn -f unveil-emf-repackaged tycho-versions:update-eclipse-metadata	
-	#mvn clean install
-	#mvn clean deploy
+	mvn versions:set -DnewVersion=${each} -DautoVersionSubmodules=true
+	mvn -f unveil-emf-repackaged tycho-versions:set-version -D newVersion=${each}
+	mvn -f unveil-emf-repackaged tycho-versions:update-eclipse-metadata
+	mvn clean install
+	mvn clean deploy
 done
-	
